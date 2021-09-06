15b97b451836e46954fab7d1fdb89f5647723b0d
Daryl <aka.hjd@gmail.com>
2021-09-07 00:06:12 +0800

    fix formatting and remove project pdf

--- example_class_2/Main.java
+++ example_class_2/Main.java
@@ -4,97 +4,82 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class Main {
+	public static void main(String[] args) {
 
-    public static void main(String[] args){
-  	
-    	final int MAXSIZE = 100;		//MAXSIZE
-    	final int MAXITERATION = 1000;	//MAXITERATION
-    	int iteration, size;
-    	long total = 0;
-    	
-    	//Header of CSV file
-    	StringBuilder sb = new StringBuilder();
+		final int MAXSIZE = 100; // MAXSIZE
+		final int MAXITERATION = 1000; // MAXITERATION
+		int iteration, size;
+		long total = 0;
+
+		// Header of CSV file
+		StringBuilder sb = new StringBuilder();
 		sb.append("Size,");
 		sb.append("Time");
 		sb.append('\n');
-		
-    	try (PrintWriter writer = new PrintWriter(new File("result.csv"))) {
-    		//start from size 2
-	    	for (size=2;size< MAXSIZE; size++) {
-	    			
-	    			//Determine range of weight of each edge
-			    	int min = 0;
-			    	int max = 15;
-			    	
-			    	//Initialise empty graph
-			    	int [][] randomWeightedGraph = new int [size][size];
-			
-			        // Create a test graph.
-			        /*int[][] graph = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
-					        		  { 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
-					        		  { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-					                  { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
-					                  { 0, 0, 0, 9, 0, 10, 0, 0, 0 }, 
-					                  { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-					                  { 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
-					                  { 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
-					                  { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-                    */
-			        
-			        //Insert random weight for each edge, within the range of min,max 
-			        for(int i=0;i<randomWeightedGraph.length;i++) {
-			        	for (int j=0;j< randomWeightedGraph.length;j++) {
-			        		int weight = ThreadLocalRandom.current().nextInt(min, max);
-			        		randomWeightedGraph[i][j] =  weight;
-			        	}
-			        	
-			        }
-			        
-			        
-			        
-			        //Run iteration to get average running time
-			        for (iteration = 0; iteration<MAXITERATION; iteration++ ) {
-			        
-                        //Start
-                        long start = System.nanoTime();
-                        AGraph a_graph = new AGraph(randomWeightedGraph);
-                        int[] result = a_graph.performDijkstra(0);
-                        
-                        //End
-                        long end = System.nanoTime();
-                    
-                        System.out.println("Distance from Source");
-                        for(int i = 0; i < randomWeightedGraph.length; ++i) {
-                            System.out.printf("%d t %d\n", i, result[i]);
-                        }
-			        
-                        //Add up total running time for MAXITERATIONS in nanaseconds
-                        total += (end-start); 
-                        System.out.println("Time taken: " + (end-start) + " nanoseconds");
-			        
-					
-			    	
-	
-			     
-	    		    }//for MAXITERATION
-			        
-			    //Append size of matrix to CSV
-			    sb.append(size);
-			    sb.append(',');
-			    
-			    //Append average running time for each size
-				sb.append(total/MAXITERATION);
-				sb.append('\n');      
+
+		try (PrintWriter writer = new PrintWriter(new File("result.csv"))) {
+			// start from size 2
+			for (size = 2; size < MAXSIZE; size++) {
+
+				// Determine range of weight of each edge
+				int min = 0;
+				int max = 15;
+
+				// Initialise empty graph
+				int[][] randomWeightedGraph = new int[size][size];
+
+				// Create a test graph.
+				/*
+				 * int[][] graph = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, { 4, 0, 8, 0, 0, 0, 0, 11, 0
+				 * }, { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, { 0, 0, 0,
+				 * 9, 0, 10, 0, 0, 0 }, { 0, 0, 4, 14, 10, 0, 2, 0, 0 }, { 0, 0, 0, 0, 0, 2, 0,
+				 * 1, 6 }, { 8, 11, 0, 0, 0, 0, 1, 0, 7 }, { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+				 */
+
+				// Insert random weight for each edge, within the range of min,max
+				for (int i = 0; i < randomWeightedGraph.length; i++) {
+					for (int j = 0; j < randomWeightedGraph.length; j++) {
+						int weight = ThreadLocalRandom.current().nextInt(min, max);
+						randomWeightedGraph[i][j] = weight;
+					}
+
+				}
+
+				// Run iteration to get average running time
+				for (iteration = 0; iteration < MAXITERATION; iteration++) {
+
+					// Start
+					long start = System.nanoTime();
+					AGraph a_graph = new AGraph(randomWeightedGraph);
+					int[] result = a_graph.performDijkstra(0);
+
+					// End
+					long end = System.nanoTime();
+
+					System.out.println("Distance from Source");
+					for (int i = 0; i < randomWeightedGraph.length; ++i) {
+						System.out.printf("%d t %d\n", i, result[i]);
+					}
+
+					// Add up total running time for MAXITERATIONS in nanaseconds
+					total += (end - start);
+					System.out.println("Time taken: " + (end - start) + " nanoseconds");
+
+				} // for MAXITERATION
+
+				// Append size of matrix to CSV
+				sb.append(size);
+				sb.append(',');
+
+				// Append average running time for each size
+				sb.append(total / MAXITERATION);
+				sb.append('\n');
 				System.out.println("done!");
-				
-	    	}//for maxSize
-	    	writer.write(sb.toString());
-			}
-    	
-    	 	catch (FileNotFoundException e) {
-		     System.out.println(e.getMessage());
-    	
-    	 	}
-    	
-    }
+
+			} // for maxSize
+			writer.write(sb.toString());
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
