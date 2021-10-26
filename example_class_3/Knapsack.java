4a54362381251dedf8f99f8efde7479459e18cde
Daryl <aka.hjd@gmail.com>
2021-10-26 22:01:51 +0800

    formatting

--- example_class_3/Knapsack.java
+++ example_class_3/Knapsack.java
@@ -1,47 +1,44 @@
 public class Knapsack {
+    public static void main(String[] args) {
+        int[] weight1 = {4, 6, 8};
+        int[] profit = {7, 6, 9};
+        int[] weight2 = {5, 6, 8};
+        Knapsack_recursive recur = new Knapsack_recursive();
+        Knapsack_dynamic dyna = new Knapsack_dynamic();
 
-	public static void main(String[] args) {
-		int[] weight1 = {4,6,8};
-	    int[] profit = {7,6,9};
-		int[] weight2 = {5,6,8};
-		Knapsack_recursive recur = new Knapsack_recursive();
-	    Knapsack_dynamic dyna = new Knapsack_dynamic();
-	    
-	    
-	    System.out.print("The weights for the objects are: "); 
-	    for (int i =0;i<profit.length;i++) {
-		    System.out.print(weight1[i] + " ");  
-	    }
-	    System.out.println();
-	    
-	    System.out.print("The profits for the objects are: ");
-	    for (int i =0;i<profit.length;i++) {
-	    	System.out.print(+ profit[i] + " ");
-	    }
-	    System.out.println();
-	   
-		System.out.println("The maximum profit for recursive programming is: " + recur.P(14,weight1.length,weight1,profit));
-		System.out.println("The maximum profit for dynamic programming is: " + dyna.knapsack(14,weight1.length,weight1,profit));
-		System.out.println();
-		
-		
-		 System.out.print("The weights for the objects are: "); 
-		 for (int i =0;i<profit.length;i++) {
-			System.out.print(weight2[i] + " ");  
-		 }
-		 
-		 System.out.println();
-		    
-		 System.out.print("The profits for the objects are: ");
-		 for (int i =0;i<profit.length;i++) {
-		    System.out.print(+ profit[i] + " ");
-		 }
-		 
-		System.out.println();
-		
-		System.out.println("The maximum profit for recursive programming is: " + recur.P(14,weight2.length,weight2,profit));
-		System.out.println("The maximum profit for dynamic programming is: " + dyna.knapsack(14,weight2.length,weight2,profit));
+        System.out.print("The weights for the objects are: ");
+        for (int i = 0; i < profit.length; i++) {
+            System.out.print(weight1[i] + " ");
+        }
+        System.out.println();
 
-	}
+        System.out.print("The profits for the objects are: ");
+        for (int i = 0; i < profit.length; i++) {
+            System.out.print(+profit[i] + " ");
+        }
+        System.out.println();
 
+        System.out.println(
+                "The maximum profit for recursive programming is: " + recur.P(14, weight1.length, weight1, profit));
+        System.out.println(
+                "The maximum profit for dynamic programming is: " + dyna.knapsack(14, weight1.length, weight1, profit));
+        System.out.println();
+
+        System.out.print("The weights for the objects are: ");
+        for (int i = 0; i < profit.length; i++) {
+            System.out.print(weight2[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("The profits for the objects are: ");
+        for (int i = 0; i < profit.length; i++) {
+            System.out.print(+profit[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println(
+                "The maximum profit for recursive programming is: " + recur.P(14, weight2.length, weight2, profit));
+        System.out.println(
+                "The maximum profit for dynamic programming is: " + dyna.knapsack(14, weight2.length, weight2, profit));
+    }
 }
--- example_class_3/Knapsack_recursive.java
+++ example_class_3/Knapsack_recursive.java
@@ -2,29 +2,23 @@ class Knapsack_recursive {
     public int row;
     public int col;
     public int C;
-    
-    public Knapsack_recursive(){
-    	
+
+    public Knapsack_recursive() {
     }
-    
-    public int P(int C, int n, int[] weight, int[] profit){ // j = number of objects
-    	
-    	int index = n-1;
-    	if (C == 0 || n == 0){
+
+    public int P(int C, int n, int[] weight, int[] profit) { // j = number of objects
+        int index = n - 1;
+        if (C == 0 || n == 0) {
             return 0;
         }
-        
-        if (weight[index] > C){
-            return P(C,index,weight,profit);
-        }
-        
-        else{
-        	return Math.max(Math.max(P(C,index,weight,profit), profit[index] + P(C - weight[index], index,weight,profit)), 
-                        Math.max(profit[index] + P(C - weight[index], index,weight,profit), profit[index] + P(C - weight[index], index+1,weight,profit)));
+
+        if (weight[index] > C) {
+            return P(C, index, weight, profit);
+        } else {
+            return Math.max(Math.max(P(C, index, weight, profit), profit[index] + P(C - weight[index], index, weight, profit)),
+                    Math.max(profit[index] + P(C - weight[index], index, weight, profit), profit[index] + P(C - weight[index], index + 1, weight, profit)));
         }
     }
-
-
 }
 
 
